bdb8314e16b8317f053ad83aac266907b43b00c7
caiohamamura <caiohamamura@gmail.com>
2023-04-14 08:17:59 -0300

    Initial commit

---
+++ Brendo_Enzo_de_Souza/java.js
@@ -0,0 +1,10 @@
+function calcula() {
+    if(media.innerHTML >= 6) {
+        document.getElementById("media").innerHTML = "Parabéns! Sua nota foi de " +media.innerHTML;
+        media.style.color='green';
+    }
+    else{
+        document.getElementById("media").innerHTML = "Se lascou! Ainda falta "+(6-media.innerHTML);
+        media.style.color = 'red';
+    }
+}
